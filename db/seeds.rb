999403d23a0f01ea017a4ec57e2c0db472b62f0b
Dan <danh@users.noreply.github.com>
2016-12-09 01:49:26 -0600

    seeds is complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-organizations_info = [
+organizations_info =
   {
     :name => "University of Chicago",
-    :descriptive_information => "Where fun comes to die"
-  },
+    :description => "Where fun comes to die"
+  }
 
 Organization.create organizations_info
 
+puts "There are now #{Organization.count} organizations in the database."
 
 
 names = ["alice smith", "bob doe", "carol johnson"]
@@ -21,11 +22,11 @@ names = ["alice smith", "bob doe", "carol johnson"]
 names.each do |name|
   name = name.scan(/\w+/)
   user = User.create
-  user.email = "#{name[1]}@example.com"
+  user.email = "#{name[0]}@example.com"
   user.password = "12341234"
   user.zipcode = '12345'
-  user.first_name = name[1]
-  user.last_name = name[2]
+  user.first_name = name[0]
+  user.last_name = name[1]
   user.save
 end
 
@@ -75,60 +76,77 @@ items_info = [
   {
     :name => "Purple Stuff",
     :descriptive_information => "not Sunny-D"
-  }
+  }]
 
-Items.create items_info
+Item.create items_info
 
-items = Itemss.all
+puts "There are now #{Item.count} items in the database."
+
+items = Item.all
 users = User.all
 organizations = Organization.all
 
 users.each do |user|
 
-  membership = user.membership.build
-  membership.user_id = user.id
+  membership = user.memberships.build
+  # membership.user_id = user.id
   membership.organization_id = organizations.sample.id
   membership.save
 
-  rand(3).times do
-    inventory = user.inventory.build
+  [3,4,5].sample.times do
+    inventory = user.inventories.build
     inventory.item_id = items.sample.id
-    inventory.user_id = user.id
-    inventory.quantity = [rand(5), 1].min
+    # inventory.user_id = user.id
+    inventory.quantity = [rand(5), 1].max
     inventory.save
   end
 
-  rand(5).times do
-    proposed_trade = user.proposed_trade.build
+  [3,4,5].sample.times do
+    proposed_trade = user.sent_proposals.build
     proposed_trade.status = ["Pending","Rejected","Accepted"].sample
-    proposed_trade.proposer_id = user.user_id
-    proposed_trade.recipient_id = users.sample.id
-    if proposed_trade.recipient_id = user.user_id || proposed_trade.recipient_id != 1
-      proposed_trade.recipient_id = proposed_trade.recipient_id - 1
-    else
-      proposed_trade.recipient_id = 1
+    # proposed_trade.proposer_id = user.id
+    loop do
+      proposed_trade.recipient_id = users.sample.id
+      break if proposed_trade.recipient_id != user.id
     end
     proposed_trade.save
   end
 
 end
 
-proposed_trades = Proposed_trades.all
+puts "There are now #{Membership.count} memberships in the database."
+puts "There are now #{Inventory.count} inventories in the database."
+puts "There are now #{ProposedTrade.count} proposed trades in the database."
+
+proposed_trades = ProposedTrade.all
 
 proposed_trades.each do |proposed_trade|
-  rand(4).times do
-    next if [1,2].sample == 1
-    comment = proposed_trade.comment.build
-    comment.proposed_trade_id = proposed_trade.idea
+  [1,2,3].sample.times do
+    next if [1,2,3,4,5].sample == 1
+    comment = proposed_trade.comments.build
+    # comment.proposed_trade_id = proposed_trade.id
     comment.user_id = [proposed_trade.proposer_id,    proposed_trade.recipient_id].sample
     body = ["Sounds ok",
       "Are you sure?",
       "I don't know if you can do any better",
       "Really?!?!?!?!",
-      "What's up bro/sis?"
+      "What's up bro/sis?",
       "This is not for reals"
     ].sample
+    comment.save
   end
-end
+
+  line_item_1 = proposed_trade.line_items.build
+  line_item_1.inventory_id = proposed_trade.proposer.inventories.sample.id
+  line_item_1.quantity = "1"
+  line_item_1.save
+
+  line_item_2 = proposed_trade.line_items.build
+  line_item_2.inventory_id = proposed_trade.recipient.inventories.sample.id
+  line_item_2.quantity = "1"
+  line_item_2.save
 
 end
+
+puts "There are now #{Comment.count} comments in the database."
+puts "There are now #{LineItem.count} line items in the database."
